ce6885a2a817f9c5090a08be518cc86abedcb549
Ted Staros <tstaros23@gmail.com>
2022-01-14 22:11:43 -0700

    Added test for working geocode service

--- app/services/map_quest_service.rb
+++ app/services/map_quest_service.rb
@@ -1,8 +1,8 @@
 class MapQuestService < BaseService
-  def self.get_service(address)
-     response = conn().get do |f|
+  def self.get_geocode(address)
+     response = conn('http://www.mapquestapi.com/geocoding/v1/address').get do |f|
        f.params['location'] = address
-       f.params[] = ENV["map_quest_key"]
+       f.params['key'] = ENV["map_quest_key"]
      end
      get_json(response)
    end
--- spec/services/map_quest_service_spec.rb
+++ spec/services/map_quest_service_spec.rb
@@ -4,12 +4,18 @@ require 'rails_helper'
    it "can get the geocode" do
      json = MapQuestService.get_geocode("Denver,CO")
 
-     expect(json[:info]).to be_a(Hash)
-     expect(json[:info]).to have_key(:statuscode)
-     expect(json[:info]).to have_key(:copyright)
-     expect(json[:info]).to have_key(:messages)
-     expect(json[:info][:copyright]).to have_key(:text)
-     expect(json[:info][:copyright]).to have_key(:imageUrl)
-     expect(json[:info][:copyright]).to have_key(:imageAltText)
+     expect(json).to have_key(:results)
+     expect(json[:results]).to be_a(Array)
+     expect(json[:results][0]).to have_key(:locations)
+     expect(json[:results][0][:locations]).to be_a(Array)
+     expect(json[:results][0][:locations][0]).to have_key(:latLng)
+     expect(json[:results][0][:locations][0][:latLng]).to have_key(:lat)
+     expect(json[:results][0][:locations][0][:latLng]).to have_key(:lng)
+     expect(json[:results][0][:locations][0][:latLng][:lng]).to eq(-104.984853)
+     expect(json[:results][0][:locations][0][:latLng][:lng]).to be_a(Float)
+     expect(json[:results][0][:locations][0][:latLng][:lat]).to eq(39.738453)
+     expect(json[:results][0][:locations][0][:latLng][:lat]).to be_a(Float)
+
+
    end
  end
