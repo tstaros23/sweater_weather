27cb359add5cb0e49037f9d7fd555eac6e6cff51
Ted Staros <tstaros23@gmail.com>
2022-01-15 13:26:54 -0700

    Add weather service test

---
+++ spec/services/weather_service_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+ RSpec.describe WeatherService do
+   it "can get the weather forecast" do
+     json = WeatherService.get_weather("Denver,CO")
+
+     expect(json).to be_a(Hash)
+     expect(json).to have_key(:current)
+     expect(json[:current]).to be_a(Hash)
+     expect(json[:current]).to have_key(:dt)
+     expect(json[:current]).to have_key(:sunrise)
+     expect(json[:current]).to have_key(:sunset)
+     expect(json[:current]).to have_key(:temp)
+     expect(json[:current]).to have_key(:feels_like)
+     expect(json[:current]).to have_key(:humidity)
+     expect(json[:current]).to have_key(:uvi)
+     expect(json[:current]).to have_key(:visibility)
+     expect(json[:current]).to have_key(:weather)
+     expect(json[:current][:weather]).to have_key(:description)
+     expect(json[:current][:weather]).to have_key(:icon)
+
+     expect(json).to have_key(:daily)
+     expect(json[:daily]).to be_a(Array)
+     expect(json[:daily][0]).to have_key(:dt)
+     expect(json[:daily][0]).to have_key(:sunrise)
+     expect(json[:daily][0).to have_key(:sunset)
+     expect(json[:daily][0).to have_key(:temp)
+     expect(json[:daily][0[:temp]).to have_key(:min)
+     expect(json[:daily][0[:temp]).to have_key(:max)
+     expect(json[:daily][0]).to have_key(:weather)
+     expect(json[:daily][0[:weather][0]).to have_key(:description)
+     expect(json[:daily][0[:weather][0]).to have_key(:icon)
+
+
+     expect(json).to have_key(:hourly)
+     expect(json[:hourly]).to be_a(Array)
+     expect(json[:hourly][0]).to have_key(:dt)
+     expect(json[:hourly][0]).to have_key(:temp)
+     expect(json[:hourly][0]).to have_key(:weather)
+     expect(json[:hourly][0][:weather][0]).to have_key(:description)
+     expect(json[:hourly][0][:weather][0]).to have_key(:icon)
+   end
+ end
