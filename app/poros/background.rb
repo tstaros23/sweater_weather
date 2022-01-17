44e60938298704bec2eeeba4eb34e51543c20b5f
Ted Staros <tstaros23@gmail.com>
2022-01-16 20:36:44 -0700

    Finish background poro

--- app/poros/background.rb
+++ app/poros/background.rb
@@ -1,5 +1,11 @@
 class Background
-  def initialize(attr)
-
+  attr_reader :id, :url, :author_name, :author_portfolio, :author_profile_url, :location, :source
+  def initialize(attr, location)
+    @url = attr[:urls][:raw]
+    @author_name = attr[:user][:name]
+    @author_portfolio = attr[:user][:portfolio_url]
+    @author_profile_url = attr[:links][:self]
+    @location = location
+    @source = 'unsplash.com'
   end
 end
