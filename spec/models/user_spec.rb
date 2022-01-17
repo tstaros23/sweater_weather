cb63cfa285ad3d5ff9f66cbe6b8ae6eca8677553
Ted Staros <tstaros23@gmail.com>
2022-01-17 11:17:03 -0700

    Fix model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,6 @@ require 'rails_helper'
      it {should validate_presence_of(:email)}
      it {should validate_uniqueness_of(:email)}
      it {should validate_presence_of(:password)}
+     it {should validate_confirmation_of(:password)}
    end
  end
