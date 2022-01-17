d5ca670f9cee3c33561fa89a6f8af5de8dc8f7e8
Ted Staros <tstaros23@gmail.com>
2022-01-17 13:24:46 -0700

    Dream drive user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'forecasts#index'
       get '/backgrounds', to: 'backgrounds#index'
+      post '/users', to: 'sessions#create'
     end
   end
 end
