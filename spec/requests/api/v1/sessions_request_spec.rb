4d27f3e9c3668e531e7fcef6d95faf82df91c905
Ted Staros <tstaros23@gmail.com>
2022-01-17 18:28:46 -0700

    Finish testing and creating user sessions api

--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -12,24 +12,24 @@ require 'rails_helper'
 
      headers = { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
 
-     post '/api/v1/users', params: {}, as: :json
+     post '/api/v1/sessions', params: { email: "whatever@example.com", password: "password"}, as: :json
 
      json = JSON.parse(response.body, symbolize_names: true)
 
      expect(response.status).to eq(200)
      expect(response).to be_successful
-
+     expect(json[:data][:attributes][:api_key]).to eq(user.api_key)
      expect(json).to have_key(:data)
      expect(json[:data]).to have_key(:type)
      expect(json[:data][:type].class).to eq(String)
      expect(json[:data]).to have_key(:id)
-     expect(json[:data][:id].class).to eq(Integer)
+     expect(json[:data][:id].class).to eq(String)
      expect(json[:data]).to have_key(:attributes)
      expect(json[:data][:attributes].class).to eq(Hash)
      expect(json[:data][:attributes]).to have_key(:email)
      expect(json[:data][:attributes][:email].class).to eq(String)
      expect(json[:data][:attributes]).to have_key(:api_key)
-     expect(json[:data][:attributes][:api_key]).to eq(String)
+     expect(json[:data][:attributes][:api_key].class).to eq(String)
    end
 
    it "will not expose confidential data" do
@@ -43,12 +43,33 @@ require 'rails_helper'
 
      headers = { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
 
-     post '/api/v1/users', params: {}, as: :json
+     post '/api/v1/sessions', params: {email: "tstaros23@gmail.com", password: "12345"}, as: :json
 
      json = JSON.parse(response.body, symbolize_names: true)
 
      expect(json).not_to have_key(:password)
      expect(json).not_to have_key(:password_confirmation)
-     expect(json).to_to have_key(:password_digest)
+     expect(json).not_to have_key(:password_digest)
+   end
+
+   it "gives an error if bad credentials are given" do
+     user = User.create!(email: "whatever@example.com", password: "password", password_confirmation: "password")
+
+     body = {
+       email: "whatever@example.com",
+       password: "boogers",
+       password_confirmation: user.password
+     }
+
+     headers = { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
+
+     post '/api/v1/sessions', params: {}, as: :json
+
+     json = JSON.parse(response.body, symbolize_names: true)
+
+     expect(response.status).to eq(400)
+     expect(response).not_to be_successful
+
+     expect(json[:errors][:details]).to eq("Bad credentials")
    end
  end
