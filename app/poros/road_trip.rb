661ff161006006202d68ffa61b7120eb049deabb
Ted Staros <tstaros23@gmail.com>
2022-01-18 21:47:06 -0700

    Update poro

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -4,7 +4,15 @@ class RoadTrip
   def initialize(attr)
     @start_city = attr[:start_city]
     @end_city = attr[:end_city]
-    @travel_time = attr[:travel_time]
+    @travel_time = is_possible?(attr)
     @weather_at_eta = attr[:weather_at_eta]
+  end
+
+  def is_possible?(attr)
+    if attr.has_key?(:distance)
+      attr[:travel_time]
+    else
+      "impossible route"
+    end
   end
 end
