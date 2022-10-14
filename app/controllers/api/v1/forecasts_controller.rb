bf5c0f0a6817d9340065d55692ec7c215e7606a0
Ted Staros <tstaros23@gmail.com>
2022-10-14 00:13:10 -0600

    Add comment

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::ForecastsController < ApplicationController
     current_weather = ForecastFacade.create_current_weather(params[:location])
     daily_weather = ForecastFacade.create_daily_weather(params[:location])
     hourly_weather = ForecastFacade.create_hourly_weather(params[:location])
+    #work on calling only one service
     render json: ForecastSerializer.format_json(current_weather, daily_weather, hourly_weather)
   end
 end
